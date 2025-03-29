3a8e2758ea2379aa5a293934e8f3139d0c8bd3b8
tmd-2003 <tmd2003@icloud.com>
2025-03-29 17:58:58 +0000

    first bash, passed test of adding tasks

--- python/todo.py
+++ python/todo.py
@@ -1,17 +1,32 @@
-import argparse
 import os
+import argparse
 
-TASK_FILE = ".tasks.txt"
+task_file = os.path.join(os.path.dirname(__file__), "test", "test_list.txt")
+
 
 def add_task(task):
     """Function: add_task
-    
+
     Input - a task to add to the list
     Return - nothing
     """
+    with open(TASK_FILE, "a", encoding="utf-8") as file:
+       file.write(task + "\n")
+       
+       return 
+      
 
 def list_tasks():
-    return
+  
+    with open(task_file, "r", encoding="utf-8") as file:
+        tasks = file.readlines()
+        counter = 1
+        output_string = ""
+        for task in tasks:
+            output_string += f"{counter}. {task}"
+            counter += 1
+
+    return output_string.rstrip()
 
 
 def remove_task(index):
